0faae69a212109866bedf76d87e4316ef1b45764
ehdgks0627 <ehdgks0627@naver.com>
2018-02-01 18:21:28 -0800

    Moved random generator funciton to utils.py

--- User/tests.py
+++ User/tests.py
@@ -1,22 +1,6 @@
 from django.test import TestCase
 from .models import Account, AccountManager
-
-
-def random_string(length=8, only_digit=False, only_lower=False):
-    import random
-    import string
-    if only_digit:
-        return ''.join(random.choice(string.digits) for _ in range(length))
-    else:
-        if only_lower:
-            return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
-        else:
-            return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
-
-
-def random_bool():
-    import random
-    return random.choice([False, True])
+from solomon.utils import *
 
 
 class QuestionMethodTests(TestCase):

dd0c7ba565b56683f9b773f6051917ee114eda05
ehdgks0627 <ehdgks0627@naver.com>
2018-02-01 17:20:02 -0800

    Added essential apps

---
+++ User/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser,
+    PermissionsMixin)
+
+
+class AccountManager(BaseUserManager):
+    # TODO add tuhmbnail field
+    def create_account(self, id, password, email, nickname, phone):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            id=id,
+            email=AccountManager.normalize_email(email),
+            nickname=nickname,
+            phone=phone,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, id, password, email, nickname, phone):
+        user = self.create_account(
+            id=id,
+            password=password,
+            email=email,
+            nickname=nickname,
+            phone=phone
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    id = models.CharField(
+        verbose_name='id',
+        primary_key=True,
+        max_length=32,
+        unique=True,
+        null=False,
+        blank=False
+    )
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    nickname = models.CharField(
+        'nickname',
+        max_length=10,
+        blank=False,
+        unique=True,
+        default='')
+    thumbnail = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='image/thumbnail/',
+    )
+    phone = models.CharField(
+        verbose_name='phone',
+        max_length=16,
+        unique=True,
+        null=False,
+        blank=False
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'id'
+    REQUIRED_FIELDS = ['nickname', 'email', 'phone']
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_superuser
---
+++ User/tests.py
@@ -0,0 +1,67 @@
+from django.test import TestCase
+from .models import Account, AccountManager
+
+
+def random_string(length=8, only_digit=False, only_lower=False):
+    import random
+    import string
+    if only_digit:
+        return ''.join(random.choice(string.digits) for _ in range(length))
+    else:
+        if only_lower:
+            return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(length))
+        else:
+            return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
+
+
+def random_bool():
+    import random
+    return random.choice([False, True])
+
+
+class QuestionMethodTests(TestCase):
+    def test_create_user(self):
+        id = random_string()
+        email = random_string() + "@" + random_string(only_lower=True) + ".com"
+        nickname = random_string()
+        phone = random_string(only_digit=True)
+        password = Account.objects.make_random_password()
+        is_active = random_bool()
+        user = Account.objects.create_account(
+            id=id, email=email, nickname=nickname, phone=phone, password=password)
+
+        self.assertNotEqual(user, None)
+        user.is_active = is_active
+        self.assertEqual(user.id, id)
+        self.assertEqual(user.email, email)
+        self.assertEqual(user.nickname, nickname)
+        self.assertEqual(user.phone, phone)
+        self.assertEqual(user.is_active, is_active)
+        self.assertEqual(user.is_staff, False)
+
+        user.delete()
+
+    def test_create_super_user(self):
+        id = random_string()
+        email = random_string() + "@" + random_string(only_lower=True) + ".com"
+        nickname = random_string()
+        phone = random_string(only_digit=True)
+        password = Account.objects.make_random_password()
+        is_active = random_bool()
+        super_user = Account.objects.create_superuser(
+            id=id, email=email, nickname=nickname, phone=phone, password=password)
+
+        self.assertNotEqual(super_user, None)
+        super_user.is_active = is_active
+        self.assertEqual(super_user.id, id)
+        self.assertEqual(super_user.email, email)
+        self.assertEqual(super_user.nickname, nickname)
+        self.assertEqual(super_user.phone, phone)
+        self.assertEqual(super_user.is_active, is_active)
+        self.assertEqual(super_user.is_staff, True)
+
+        super_user.delete()
+
+
+class QuestionViewTests(TestCase):
+    pass

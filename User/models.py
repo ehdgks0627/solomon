515e203695fe6dbeef604855f7990f04485d882d
sprout <ehdgks0627>
2018-02-01 18:23:38 -0800

    Added date field in Account

--- User/models.py
+++ User/models.py
@@ -67,6 +67,8 @@ class Account(AbstractBaseUser, PermissionsMixin):
         null=False,
         blank=False
     )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     is_active = models.BooleanField(default=True)
 

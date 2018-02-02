91b944325b695d9a2ef4b4b6b6e64dd88d1cc18c
sprout <ehdgks0627>
2018-02-01 18:29:12 -0800

    Added Porject model

--- Project/models.py
+++ Project/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    title = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False
+    )
+
+    description = models.CharField(
+        max_length=8192,
+        blank=False,
+        null=False
+    )
+
+    price = models.IntegerField(
+        blank=False,
+        null=False
+    )
+
+    period = models.IntegerField(
+        blank=False,
+        null=False
+    )
+
+    REQUIRED_FIELDS = ['title', 'description', 'price', 'period']

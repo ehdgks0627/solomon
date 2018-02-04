e06540c6ef5b51cc1e57c2ba3e85e6fcb666d0cd
sprout <ehdgks0627>
2018-02-04 15:28:00 -0800

    Implemented register & logout

--- User/views.py
+++ User/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.views.decorators.http import require_http_methods
+from django.db import IntegrityError
+from User.models import Account
 
 
 @require_http_methods(['POST'])
@@ -18,3 +20,22 @@ def login_view(request):
 def logout_view(request):
     logout(request=request)
     return redirect('/')
+
+
+@require_http_methods(['POST'])
+def register_view(request):
+    try:
+        user = Account.objects.create_user(id=request.POST.get('uid'),
+                                           password=request.POST.get('upw'),
+                                           email=request.POST.get('uemail'),
+                                           nickname=request.POST.get('unickname'),
+                                           phone=request.POST.get('uphone'))
+    except IntegrityError:
+        user = None
+    if user is not None:
+        login(request, user)
+        # TODO register success
+        return redirect('/')
+    else:
+        # TODO register error
+        return redirect('/')

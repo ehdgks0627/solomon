de8e8c5a0439e7d4b75c94babd582a6f1b6e57ad
ehdgks0627 <ehdgks0627@naver.com>
2018-02-04 22:19:26 -0800

    Changed sub-navbar font color

--- static/js/base.js
+++ static/js/base.js
@@ -7,7 +7,7 @@ $(function () {$.ajax({
         $.each(data, function (key, element) {
             var li = $('<li>', {'class': 'nav-item dropdown'});
             li.append($('<a>', {'class': 'nav-link dropdown-toggle', 'id': 'category' + count, 'data-toggle': 'dropdown',
-                                'aria-haspopup': 'true', 'aria-expanded': 'true', 'style': 'color: black;', 'text': key}));
+                                'aria-haspopup': 'true', 'aria-expanded': 'true', 'style': 'color: #383d41;', 'text': key}));
             var div = $('<div>', {'class': 'dropdown-menu', 'aria-labelledby': 'category' + count})
             $.each(element, function (key, element) {
                 //TODO element, 네임 설정

8676cdd099ac17c411205f0ec19d27dd97c5895b
ehdgks0627 <ehdgks0627@naver.com>
2018-02-05 22:46:59 -0800

    Added tool to reset database

---
+++ reset_db.sh
@@ -0,0 +1,17 @@
+rm -r Contact/migrations
+rm -r Contract/migrations
+rm -r Order/migrations
+rm -r Product/migrations
+rm -r Project/migrations
+rm -r Review/migrations
+rm -r User/migrations
+rm db.sqlite3
+python manage.py makemigrations
+python manage.py makemigrations Contact
+python manage.py makemigrations Contract
+python manage.py makemigrations Order
+python manage.py makemigrations Product
+python manage.py makemigrations Project
+python manage.py makemigrations Review
+python manage.py makemigrations User
+python manage.py migrate

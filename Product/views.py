5fb28ec6e0cb1b8fb08644f39913947f2fd85f47
ehdgks0627 <ehdgks0627@naver.com>
2018-02-05 21:53:39 -0800

    Implemented base view

--- Product/views.py
+++ Product/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http.response import JsonResponse
 from django.views.decorators.http import require_http_methods
-from .models import Category
+from .models import Category, Product
 
+@require_http_methods(['GET'])
+def add_product(request):
+    #TODO
+    return render(request, 'product/add.html')
+
+@require_http_methods(['GET'])
+def delete_product(request):
+    #TODO
+    return render(request, 'product/delete.html')
+
+@require_http_methods(['GET'])
+def detail_product(request):
+    return render(request, 'product/detail.html')
+
+@require_http_methods(['GET'])
+def edit_product(request):
+    #TODO
+    return render(request, 'product/edit.html')
+
+@require_http_methods(['GET'])
+def get_product(request, category=None):
+    if category:
+        products = Product.objects.filter(category=category)
+    else:
+        products = Product.objects.all()
+    return render(request, 'product/get.html', {'products': products})
 
 @require_http_methods(['GET'])
 def get_categories(request):

aa06ef2a12af3b288ec04778b121b5cff307e8f7
ehdgks0627 <ehdgks0627@naver.com>
2018-02-06 01:19:25 -0800

    Fixed some error

--- Contract/views.py
+++ Contract/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http.response import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from Contract.models import Contract
@@ -7,19 +6,19 @@ from Order.models import Order
 
 
 @require_http_methods(['GET'])
-def detail_contract(request):
+def detail_contract(request, contract_id):
     return render(request, 'contract/detail.html')
 
 
 @login_required(login_url='/')
 @require_http_methods(['GET'])
-def edit_contract(request):
-    #TODO is request.user has permission?
+def edit_contract(request, contract_id):
+    # TODO is request.user has permission?
     order_id = request.GET.get("order_id")
     order = Order.objects.filter(id=order_id)
     contract = Contract.objects.filter(order=order)
     if request.method == 'GET':
-        return render('contract/edit.html', data={'contract': contract})
+        return render('contract/edit.html', {'contract': contract})
     elif request.method == 'POST':
         # TODO
         raise NotImplementedError
@@ -29,9 +28,7 @@ def edit_contract(request):
 
 
 @require_http_methods(['GET'])
-def form_contract(request, category=None):
-    if category:
-        contracts = contract.objects.filter(category=category)
-    else:
-        contracts = contract.objects.all()
-    return render(request, 'contract/get.html', {'contracts': contracts})
+def form_contract(request, contract_id):
+    # TODO is request.user has permission?
+    contract = Contract.objects.filter(id=contract_id)
+    return render(request, 'contract/form.html', {'contract': contract})

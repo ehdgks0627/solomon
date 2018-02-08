724b442a229ab6215250c4e6f4bed937726edff2
sprout <ehdgks0627>
2018-02-08 14:40:28 -0800

    Redirected to referer page

--- User/views.py
+++ User/views.py
@@ -10,16 +10,16 @@ def login_view(request):
     user = authenticate(username=request.POST.get('uid'), password=request.POST.get('upw'))
     if user is not None:
         login(request, user)
-        return redirect('/')
+        return redirect(request.META.get('HTTP_REFERER', '/'))
     else:
         # TODO login error
-        return redirect('/')
+        return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
 @require_http_methods(['GET'])
 def logout_view(request):
     logout(request=request)
-    return redirect('/')
+    return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
 @require_http_methods(['POST'])

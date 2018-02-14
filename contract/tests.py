10b3916218ae9b8819ec89734f2593880d8493f3
ehdgks0627 <ehdgks0627@naver.com>
2018-02-13 16:56:41 -0800

    Added contract test code & Fixed clause add / del function errors

--- contract/tests.py
+++ contract/tests.py
@@ -43,7 +43,86 @@ class QuestionMethodTests(TestCase):
         self.order.save()
 
     def test_create_contract(self):
-        pass
+        buyer_agreement = random_bool()
+        seller_agreement = random_bool()
+        buyer_signature = random_string()
+
+        contract = Contract.objects.create_contract(buyer_agreement=buyer_agreement,
+                                                    seller_agreement=seller_agreement,
+                                                    buyer_signature=buyer_signature)
+
+        self.assertEqual(contract.buyer_agreement, buyer_agreement)
+        self.assertEqual(contract.seller_agreement, seller_agreement)
+
+        for _ in range(20):
+            contract.buyer_agreement = random_bool()
+            contract.seller_agreement = random_bool()
+            contract.save()
+            contract.reset_agreement()
+            self.assertEqual(contract.buyer_agreement, False)
+            self.assertEqual(contract.seller_agreement, False)
+
+        self.assertEqual(contract.clause['max_id'], 1)
+        self.assertEqual(len(contract.clause['clauses']), 0)
+
+        self.assertEqual(contract.add_clause(1, "A"), False)
+        self.assertEqual(contract.clause['max_id'], 1)
+        self.assertEqual(len(contract.clause['clauses']), 0)
+
+        self.assertEqual(contract.add_step_clause('1', 12.34), True)
+        self.assertEqual(contract.clause['max_id'], 1)
+        self.assertEqual(len(contract.clause['clauses']), 1)
+
+        self.assertEqual(contract.clause['clauses']['1']['fee'], 12.34)
+        self.assertEqual(len(contract.clause['clauses']['1']['data']), 0)
+
+        self.assertEqual(contract.add_step_clause('1', 12.34), False)
+        self.assertEqual(contract.clause['max_id'], 1)
+        self.assertEqual(len(contract.clause['clauses']), 1)
+
+        self.assertEqual(contract.clause['clauses']['1']['fee'], 12.34)
+        self.assertEqual(len(contract.clause['clauses']['1']['data']), 0)
+
+        get_content = lambda item: item['content']
+
+        before_max_id = contract.clause['max_id']
+        content = random_string()
+        self.assertEqual(contract.add_clause('1', content), True)
+        self.assertEqual(content in map(get_content, contract.clause['clauses']['1']['data']), True)
+        self.assertEqual(len(contract.clause['clauses']['1']['data']), 1)
+        self.assertEqual(before_max_id + 1, contract.clause['max_id'])
+
+        self.assertEqual(contract.add_clause('2', content), False)
+        self.assertEqual(content in map(get_content, contract.clause['clauses']['1']['data']), True)
+        self.assertEqual(len(contract.clause['clauses']['1']['data']), 1)
+        self.assertEqual(before_max_id + 1, contract.clause['max_id'])
+
+        for step in range(20):
+            before_max_id = contract.clause['max_id']
+            content = random_string()
+            self.assertEqual(contract.add_clause('1', content), True)
+            self.assertEqual(content in map(get_content, contract.clause['clauses']['1']['data']), True)
+            self.assertEqual(len(contract.clause['clauses']['1']['data']), 2 + step)
+            self.assertEqual(before_max_id + 1, contract.clause['max_id'])
+
+        self.assertEqual(contract.del_clause(0), False)
+        self.assertEqual(contract.del_clause(1), True)
+        self.assertEqual(contract.del_clause(1), False)
+        self.assertEqual(contract.del_clause(-1), False)
+
+        before_length = len(contract.clause['clauses']['1']['data'])
+        self.assertEqual(contract.del_clause(1), False)
+        self.assertEqual(len(contract.clause['clauses']['1']['data']), before_length)
+        self.assertEqual(contract.del_clause(2), True)
+        self.assertEqual(len(contract.clause['clauses']['1']['data']), before_length - 1)
+
+        self.assertEqual(contract.add_step_clause('2', 34), True)
+        self.assertEqual(contract.add_step_clause('3', 35), True)
+        self.assertEqual(contract.add_step_clause('4', 36), True)
+
+        self.assertEqual(contract.add_clause(5, content), False)
+
+        self.assertEqual(len(contract.clause['clauses']), 4)
 
 
 class QuestionViewTests(TestCase):

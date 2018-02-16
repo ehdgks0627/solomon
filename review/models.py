b3f264e5c693878342940fca38859227deb141c0
sprout <ehdgks0627>
2018-02-15 17:37:44 -0800

    Added ReviewManager & Rewrited tags field

--- review/models.py
+++ review/models.py
@@ -1,18 +1,20 @@
 from django.db import models
+import ujson
+
+
+class ReviewManager(models.Manager):
+    def create_review(self, tags=None, **kwargs):
+        order = self.model(**kwargs)
+        if tags:
+            order.tags = tags
+        order.save()
+        return order
 
 
 class Review(models.Model):
     owner = models.ForeignKey(
         'user.Account',
         related_name='%(app_label)s_%(class)s_owner',
-        on_delete=models.CASCADE,
-        blank=False,
-        null=False
-    )
-
-    product = models.ForeignKey(
-        'product.Product',
-        related_name='%(app_label)s_%(class)s_product',
         on_delete=models.CASCADE,
         blank=False,
         null=False
@@ -35,12 +37,19 @@ class Review(models.Model):
         null=False
     )
 
-    tags = models.TextField(
-        max_length=256,
-        blank=True,
+    _tags = models.TextField(
+        blank=False,
         null=False,
         default='[]'
     )
+
+    @property
+    def tags(self):
+        return ujson.loads(self._tags)
+
+    @tags.setter
+    def tags(self, value):
+        self._tags = ujson.dumps(value)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)

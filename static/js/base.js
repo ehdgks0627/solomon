f5876eebf6d629d6b1a92479b6423e241c4bbd48
sprout <ehdgks0627>
2018-02-19 13:40:19 -0800

    Added href in menu button

--- static/js/base.js
+++ static/js/base.js
@@ -10,7 +10,7 @@ $(function () {
         $.each(data, function (key, element) {
             var li = $('<li>', {'class': 'nav-item dropdown'});
             li.append($('<a>', {'class': 'nav-link dropdown-toggle', 'id': 'category' + count, 'data-toggle': 'dropdown',
-                                'aria-haspopup': 'true', 'aria-expanded': 'true', 'style': 'color: #383d41;', 'text': key}));
+                                'aria-haspopup': 'true', 'aria-expanded': 'true', 'style': 'color: #383d41;', 'text': key, 'href': '#'}));
             var div = $('<div>', {'class': 'dropdown-menu', 'aria-labelledby': 'category' + count})
             $.each(element, function (key, element) {
                 div.append($('<a>', {'class': 'dropdown-item', 'href': '#', 'href': '/product/' + element[0] + '/','text': element[1]}));
